908f677bcf9eda470a3bb60a48232e01e8070135
ridhamezrigui <ridhamezrigui07@gmail.com>
2022-10-18 15:41:23 +0100

    github actions: test the container hst deployment into AWS s3

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -4,8 +4,6 @@ import MarketingApp from './components/MarketingApp';
 export default () => {
   return (
     <div>
-      <h1>Hi there!!!!!!!!</h1>
-      <hr />
       <MarketingApp />
     </div>
   );
