061eeb230b39e3212dc4472d3ae034800eaa6ee0
ridhamezrigui <ridhamezrigui07@gmail.com>
2022-10-18 19:24:28 +0100

    Add cloudfront invalidation for index.html file

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -4,6 +4,7 @@ import MarketingApp from './components/MarketingApp';
 export default () => {
   // this is app
   return (
+    <h1>Hello from Container</h1>
     <div>
       <MarketingApp />
     </div>
