75bdead5803768fc206d250d5b515179ceb0c29c
ridhamezrigui <ridhamezrigui07@gmail.com>
2022-10-18 20:05:28 +0100

    Add the header component in the conatiner host

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import Header from './components/Header';
 import MarketingApp from './components/MarketingApp';
 
 export default () => {
   // this is app
   return (
-    <div>
-      <h1>Hello from Container</h1>
-      <MarketingApp />
-    </div>
+    <BrowserRouter>
+      <div>
+        <Header />
+        <MarketingApp />
+      </div>
+    </BrowserRouter>
   );
 };
