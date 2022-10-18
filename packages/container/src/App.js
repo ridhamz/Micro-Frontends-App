74059d4a66309ca359043589e48e557dfdc32ebd
ridhamezrigui <ridhamezrigui07@gmail.com>
2022-10-18 14:16:22 +0100

    Production configs foor the marketing app and the container host app

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import MarketingApp from './components/MarketingApp';
 
-function App() {
-  return <MarketingApp />;
-}
-
-export default App;
+export default () => {
+  return (
+    <div>
+      <h1>Hi there!</h1>
+      <hr />
+      <MarketingApp />
+    </div>
+  );
+};
--- packages/container/src/components/MarketingApp.js
+++ packages/container/src/components/MarketingApp.js
@@ -1,12 +1,12 @@
-import React, { useRef, useEffect } from 'react';
 import { mount } from 'marketing/MarketingApp';
+import React, { useRef, useEffect } from 'react';
 
-export default function MarketingApp() {
+export default () => {
   const ref = useRef(null);
 
   useEffect(() => {
     mount(ref.current);
-  }, []);
+  });
 
-  return <div ref={ref}></div>;
-}
+  return <div ref={ref} />;
+};
